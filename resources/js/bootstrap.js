48555ad9f08b27f7cd1f6873a36a9fa22600eb55
hamed gh <hamedgh3285@gmail.com>
2023-12-22 09:55:40 +0330

    import popper , jquery , bootstrap in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,6 +4,12 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+try{
+    window.Popper = require('popper.js').default;
+    window.$ = window.jquery = require('jquery');
+    require('bootstrap');
+}catch (e) {}
+
 import axios from 'axios';
 window.axios = axios;
 
